cb37406ad90ef23180e2f4835194c9cf619e181c
HarshPratapSingh1 <harshpratap715@gmail.com>
2025-12-18 05:27:10 +0530

    3

--- Backend/app.js
+++ Backend/app.js
@@ -3,12 +3,19 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectToDB = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+
+connectToDB();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+app.use('/users', userRoutes);
 // const PORT = process.env.PORT || 3000;
 // app.listen(PORT, () => {
 //     console.log(`Server is running on port ${PORT}`);
